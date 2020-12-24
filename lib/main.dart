03153c15695f26a653cb7776b8a67895dd4dcbec
SerdarAtesc <50891117+SerdarAtesc@users.noreply.github.com>
2020-12-25 02:56:28 +0300

    Main page
    Main page design

--- lib/main.dart
+++ lib/main.dart
@@ -9,109 +9,137 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'SociaWorld',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AnaSayfa(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class AnaSayfa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        backgroundColor: Colors.grey[100],
+        elevation: 0.0,
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            color: Colors.grey,
+            iconSize: 32.0,
+            onPressed: () {}),
+        title: Text(
+          "SociaWorld",
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.grey,
+          ),
         ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.notifications),
+              color: Colors.purple[300],
+              iconSize: 32.0,
+              onPressed: () {})
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 100.0,
+            decoration: BoxDecoration(color: Colors.grey[100], boxShadow: [
+              BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0.0, 3.0),
+                  blurRadius: 5.0),
+            ]),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              //Aktif kullanıcıların ana sayfada gözükmesi
+              children: <Widget>[
+                profilKartiOlustur("sadasd",
+                    "https://scontent.fyei1-1.fna.fbcdn.net/v/t1.0-9/17425984_1058356274268323_7772644928856580186_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=SE_LU7ExQ24AX8ZbTI3&_nc_ht=scontent.fyei1-1.fna&oh=8890773647ea9cc969dc024e8b959369&oe=6009F242"),
+                profilKartiOlustur("fgdgdfg",
+                    "https://scontent.fyei1-1.fna.fbcdn.net/v/t1.0-9/18670935_10209832187580744_8512526941812664066_n.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_ohc=KkcCOuDA7lQAX86bsxe&_nc_ht=scontent.fyei1-1.fna&oh=3fbddf76e9a91c802dfe96d1d37ae965&oe=600B64D2"),
+                profilKartiOlustur("fghfghgf",
+                    "https://media.discordapp.net/attachments/791056346719715380/791060954648805406/998ab29812286778.jpg"),
+                profilKartiOlustur("fghfghfg",
+                    "https://scontent.fyei1-1.fna.fbcdn.net/v/t1.0-9/17425984_1058356274268323_7772644928856580186_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=SE_LU7ExQ24AX8ZbTI3&_nc_ht=scontent.fyei1-1.fna&oh=8890773647ea9cc969dc024e8b959369&oe=6009F242"),
+                profilKartiOlustur("nfghfgh",
+                    "https://scontent.fyei1-1.fna.fbcdn.net/v/t1.0-9/17425984_1058356274268323_7772644928856580186_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=SE_LU7ExQ24AX8ZbTI3&_nc_ht=scontent.fyei1-1.fna&oh=8890773647ea9cc969dc024e8b959369&oe=6009F242"),
+                profilKartiOlustur("trtyrty",
+                    "https://scontent.fyei1-1.fna.fbcdn.net/v/t1.0-9/17425984_1058356274268323_7772644928856580186_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=SE_LU7ExQ24AX8ZbTI3&_nc_ht=scontent.fyei1-1.fna&oh=8890773647ea9cc969dc024e8b959369&oe=6009F242"),
+                profilKartiOlustur("ertert",
+                    "https://scontent.fyei1-1.fna.fbcdn.net/v/t1.0-9/17425984_1058356274268323_7772644928856580186_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=SE_LU7ExQ24AX8ZbTI3&_nc_ht=scontent.fyei1-1.fna&oh=8890773647ea9cc969dc024e8b959369&oe=6009F242"),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Material(
+              elevation: 1.0,
+              borderRadius: BorderRadius.circular(12.0),
+              child: Container(
+                padding: EdgeInsets.all(15.0),
+                width: double.infinity,
+                height: 380.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget profilKartiOlustur(String kullaniciAdi, String resimLinki) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+      child: Column(
+        children: <Widget>[
+          Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Container(
+                width: 70.0,
+                height: 70.0,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: 2.0, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(35.0),
+                    image: DecorationImage(
+                        image: NetworkImage(resimLinki), fit: BoxFit.cover)),
+              ),
+              Container(
+                width: 20.0,
+                height: 20.0,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(25),
+                    border: Border.all(width: 2.0, color: Colors.white)),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          Text(
+            kullaniciAdi,
+            style: TextStyle(fontSize: 15.0, color: Colors.black),
+          )
+        ],
+      ),
     );
   }
 }
